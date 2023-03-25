ed4d7177c8323ceb258fdbb8b86357b4c8e488a4
Magicoder <magicoder20@gmail.com>
2023-03-25 02:46:06 -0700

    Only turn on trace and video during test retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,8 +45,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-    video: 'on',
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
